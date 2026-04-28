ced442b325015a6844b1ceeed5cdb3e6fc5824fb
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-04-28 22:05:55 +0530

    remove target and add color crate

--- target/debug/deps/wordle-d041992e152796ae.d
+++
@@ -1,5 +0,0 @@
-/Users/manojseetaramgowda/projects/wordle/target/debug/deps/wordle-d041992e152796ae.d: src/main.rs
-
-/Users/manojseetaramgowda/projects/wordle/target/debug/deps/libwordle-d041992e152796ae.rmeta: src/main.rs
-
-src/main.rs:
